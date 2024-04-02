68f4dbd254f972cb2ee4f01a2ddb402ac5a2cf34
Roberto Costa <roberto.araujo339@gmail.com>
2024-04-02 11:52:01 -0300

    fisrt commit

---
+++ compile.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+emcc -O3 -s WASM=1 -s EXPORTED_RUNTIME_METHODS='["cwrap"]' \
+    -I libwebp \
+    webp.c \
+    libwebp/src/{dec,dsp,demux,enc,mux,utils}/*.c \
+    libwebp/sharpyuv/*.c
