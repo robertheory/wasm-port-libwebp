687d6f6d3e1f6ff6c47b92c8bc5f953ef80e9776
Roberto Costa <roberto.araujo339@gmail.com>
2024-04-07 20:44:24 -0300

    Fix missing function declaration in webp.c

--- webp.c
+++ webp.c
@@ -3,12 +3,6 @@
 #include <stdlib.h> // required for malloc definition
 
 EMSCRIPTEN_KEEPALIVE
-int version()
-{
-  return WebPGetEncoderVersion();
-}
-
-EMSCRIPTEN_KEEPALIVE
 uint8_t *create_buffer(int width, int height)
 {
   return malloc(width * height * 4 * sizeof(uint8_t));
